f5b44200c997d5772251d727c587a3c5f261585c
tushar <pethkartusharnarendra@gmail.com>
2025-12-30 22:44:36 +0530

    sign out in mobile

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -425,14 +425,28 @@ const Dashboard: React.FC = () => {
               </div>
               <div className="mt-3 space-y-1 px-2">
                 {userNavigation.map((item) => (
-                  <DisclosureButton
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100"
-                  >
-                    {item.name}
-                  </DisclosureButton>
+                  <>
+                    {item.name === "Sign out" ? (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        onClick={handleSignOut}
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ) : (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    )}
+                  </>
                 ))}
               </div>
             </div>
